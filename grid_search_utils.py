e76e2646313dee66e33b34273d349388fdd7739e
FABIO MARCHESE s300472 <s300472@studenti.polito.it>
2025-12-29 12:52:04 +0100

    aggiunti i parametri restanti alla classe grid, ora l'increase_rate fa parte della grid search

--- grid_search_utils.py
+++ grid_search_utils.py
@@ -23,6 +23,10 @@ class ADRGridSearch:
         seed,
         timesteps,
         architectures,
+        increase_rate,
+        learning_rate,
+        lr_scheduler_type,
+        starting_adr_range,
     ):
         self.train_env_id = train_env_id
         self.test_env_id = test_env_id
@@ -32,10 +36,16 @@ class ADRGridSearch:
         self.seed = seed
         self.timesteps = timesteps
         self.architectures = architectures
+        self.increase_rate = increase_rate
+        self.learning_rate = learning_rate
+        self.lr_scheduler_type = lr_scheduler_type
+        self.starting_adr_range = starting_adr_range
+        
         
         self.obj_adr_values = np.arange(obj_adr_range[0], obj_adr_range[1] + obj_adr_range[2], obj_adr_range[2])
         self.reward_threshold_values = np.arange(reward_threshold_range[0], reward_threshold_range[1] + reward_threshold_range[2], reward_threshold_range[2])
         self.check_freq_values = np.arange(check_freq_range[0], check_freq_range[1] + check_freq_range[2], check_freq_range[2])
+        self.increase_values = np.arange(increase_rate[0], increase_rate[1] + increase_rate[2], increase_rate[2])
 
     def run_search(self):
         """ """
@@ -46,15 +56,16 @@ class ADRGridSearch:
                 self.reward_threshold_values,
                 self.check_freq_values,
                 self.architectures,
+                self.increase_values
             )
         )
         
         pbar = tqdm(combinations, desc="Total Progress", unit="exp")
         
-        for obj_adr, reward_threshold, check_freq, arch in pbar:
-            print(f"Running experiment with Objective ADR: {obj_adr}, Reward threshold: {reward_threshold}, Check freq: {check_freq}")
+        for obj_adr, reward_threshold, check_freq, arch, increase_rate in pbar:
+            print(f"Running experiment with Objective ADR: {obj_adr}, Increase rate: {increase_rate}, Reward threshold: {reward_threshold}, Check freq: {check_freq}")
 
-            model_name = f"ppo_source_adr_{obj_adr:.2f}_{reward_threshold}_{check_freq}_{arch}_{self.seed}"
+            model_name = f"ppo_source_adr_{increase_rate:.2f}_{obj_adr:.2f}_{reward_threshold}_{check_freq}_{arch}_{self.seed}"
 
             # serve per skippare se l'esperimento esiste già
             if os.path.exists("grid_search_results.csv"):
@@ -67,12 +78,12 @@ class ADRGridSearch:
             PPO_train_adr(
                 train_env_id=self.train_env_id,
                 model_name=model_name,
-                lr=3e-4,
-                lr_scheduler_type="constant",
+                lr=self.learning_rate,
+                lr_scheduler_type=self.lr_scheduler_type,
                 steps=self.timesteps,
-                starting_adr_range=0.05,
+                starting_adr_range=self.starting_adr_range,
                 objective_adr_range=obj_adr,
-                increase_rate=0.05,
+                increase_rate=increase_rate,
                 reward_to_check=reward_threshold,
                 check_frequency=int(check_freq),
                 net_size=arch,
@@ -84,6 +95,10 @@ class ADRGridSearch:
             new_result = {
                 "model_id": model_name,
                 "architecture": arch,
+                "lr_scheduler_type": self.lr_scheduler_type,
+                "learning_rate": self.learning_rate,
+                "starting_adr_range": self.starting_adr_range,
+                "increase_rate": increase_rate,
                 "obj_adr_range": obj_adr,
                 "reward_threshold": reward_threshold,
                 "check_frequency": check_freq,
